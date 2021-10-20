97327877244b8985ffe7bfc0d1edffafd47a8a55
Nestor <nachacin@ucalgary.ca>
2021-10-20 10:56:13 -0600

    First commit for Driver Project 3
    Copied driver project 2 to make driver project 3. I expect it will be a
    good place to start.

---
+++ driver-project-3.X/gpio.h
@@ -0,0 +1,21 @@
+/* 
+ * File: 
+ * Course:    ENCM 511
+ * Section:   L02 - B04
+ * Group:     4
+ * Author: Yahia Abrini, Nasih Nazeem, Nestor Chacin
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef GPIO_H
+#define	GPIO_H
+
+#include "xc.h"
+
+/* This function is used to initialize all the input and outpin pins. Physical
+resistors were used for pull-up and pull-down purposes. */
+
+void io_init(void);
+
+#endif
---
+++ driver-project-3.X/interrupts.c
@@ -0,0 +1,42 @@
+/*
+ * Course:    ENCM 511
+ * Section:   L02 - B04
+ * Group:     4
+ * Author: Yahia Abrini, Nasih Nazeem, Nestor Chacin
+ */
+
+#include "interrupts.h"
+
+int PB1_push; // initializing PB1_push; variable holds most recent state of button
+int PB2_push; // initializing PB2_push; variable holds most recent state of button
+int PB3_push; // initializing PB3_push; variable holds most recent state of button
+int CN0flag;  // Will represent a recent change of the RB4/CN1 pin
+int CN1flag;  // Will represent a recent change of the RB4/CN1 pin
+int CN30flag; // Will represent a recent change of the RA2/CN30 pin
+
+
+void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void) {
+     IFS0bits.T2IF = 0; //Clear timer 2 interrupt flag
+     return;
+}
+
+void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void) {
+    if (IFS1bits.CNIF == 1) {
+        PB1_push = 0; // set PB1_push to 0
+        PB2_push = 0; // set PB2_push to 0
+        PB3_push = 0; // set PB3_push to 0
+        if (PORTBbits.RB4 == 0) {
+            PB1_push = 1; // set PB1_push to 1
+        }
+        if (PORTAbits.RA4 == 0) {
+            PB2_push = 1; // set PB2_push to 1
+        }
+        if (PORTAbits.RA2 == 0) {
+            PB3_push = 1; // set PB3_push to 1
+        }
+    }
+    /* Clear input change notification interrupt flag status bit */
+    IFS1bits.CNIF = 0;
+    Nop();
+    return;
+}
